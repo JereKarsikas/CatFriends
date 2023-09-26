2ba629568836282bcf31c0b473ac3ac955509417
Jere Karsikas <jere.karsikas@rentoinnovations.fi>
2023-09-26 15:15:55 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <h1>CatFriends</h1>
+    <p>djkhfdsa</p>
     <App />
   </React.StrictMode>
 );
